2932efe1bfd6ab306783c6d114556865dd2c71d3
Alexander Mukas <alexandermukas@yahoo.com>
2016-07-20 19:57:16 +0300

    Create py file

---
+++ task.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+var_string = "011101000001"
+
+def main(var_string):
+    first = ""
+    z = 0
+    zmax = 0
+    a = 0
+    for i in var_string:
+        if (first != i) or (i == "0"):
+            first = i
+            if (i == "1"):
+                a += 1
+
+            if (a == 1) and (i == "0"):
+                z += 1
+
+            if (a == 2):
+                a = 1
+                if (zmax < z):
+                    zmax = z
+                z=0
+        else:
+            continue
+    return zmax
+
+result = 'Максимальное кол-во нулей между единиц = ' + str(main(var_string))
+
+print("Значение -", var_string)
+print(result)
